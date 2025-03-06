3190591686c099b3932b61d0b2f5edbcb934b461
aldoht <aldofernandoht@gmail.com>
2025-03-05 20:01:32 -0600

    feat(header): create header layout with logos

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import {IconComponent} from "../icon/icon.component";
 
 @Component({
   selector: 'app-header',
-  imports: [],
+    imports: [
+        IconComponent
+    ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
