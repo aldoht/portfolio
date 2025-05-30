d7097204b4514205b87b01f5ad810896ab77ccef
aldoht <aldofernandoht@gmail.com>
2025-05-30 00:45:34 -0600

    feat(content): add almost all of the webpage's contet

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import {RepositoryGroupComponent} from '../repository-group/repository-group.component';
 
 @Component({
   selector: 'app-content',
-  imports: [],
+  imports: [
+    RepositoryGroupComponent
+  ],
   templateUrl: './content.component.html',
   styleUrl: './content.component.css'
 })
