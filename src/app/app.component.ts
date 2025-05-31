2763f31de286c8a2f020c5895f9298007dc562dc
aldoht <aldofernandoht@gmail.com>
2025-05-31 00:45:29 -0600

    feat(app): update structure with the actual components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {RepositoryComponent} from './repository/repository.component';
-import {RepositoryGroupComponent} from './repository-group/repository-group.component';
-import {IconComponent} from './icon/icon.component';
 import {HeaderComponent} from './header/header.component';
 import {MainContentComponent} from './main-content/main-content.component';
+import {ContentComponent} from './content/content.component';
+import {FooterComponent} from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RepositoryComponent, RepositoryGroupComponent, IconComponent, HeaderComponent, MainContentComponent],
+  imports: [RouterOutlet, HeaderComponent, MainContentComponent, ContentComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
